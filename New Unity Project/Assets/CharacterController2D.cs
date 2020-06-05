11e5aef3c7da20dc3821fc5e92040c9451d2bbed
Max Hobday <mhobday@unomaha.edu>
2020-06-04 19:30:33 -0500

    grappling hook kinda works

--- New Unity Project/Assets/CharacterController2D.cs
+++ New Unity Project/Assets/CharacterController2D.cs
@@ -3,8 +3,9 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-	[SerializeField] private float JumpForce = 400f;							// Amount of force added when the player jumps.
-	[Range(0, .3f)] [SerializeField] private float MovementSmoothing = .05f;	// How much to smooth out the movement
+	[SerializeField] private GameObject player;
+	[SerializeField] private float JumpForce = 40f;							// Amount of force added when the player jumps.
+	[Range(0, 1f)] [SerializeField] public float MovementSmoothing = .05f;	// How much to smooth out the movement
 	[SerializeField] private bool AirControl = true;							// Whether or not a player can steer while jumping;
 	[SerializeField] private LayerMask GroundLayer;							    // A mask determining what is ground to the character
 	[SerializeField] private Transform GroundCheck;							    // A position marking where to check if the player is grounded.
@@ -12,8 +13,11 @@ public class CharacterController2D : MonoBehaviour
 	const float GroundedRadius = .2f;  // Radius of the overlap circle to determine if grounded
 	private bool Grounded;            // Whether or not the player is grounded.
 	private Rigidbody2D body;
-	private bool FacingRight = true;  // For determining which way the player is currently facing.
+	[SerializeField] private SpriteRenderer spriteRenderer;
+	private static bool FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 Velocity = Vector3.zero;
+	//The grappling hook
+	DistanceJoint2D hook;
 
 	[Header("Events")]
 	[Space]
@@ -25,7 +29,9 @@ public class CharacterController2D : MonoBehaviour
 
 	private void Awake()
 	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
 		body = GetComponent<Rigidbody2D>();
+		hook = GetComponent<DistanceJoint2D>();
 
 		if (OnLandEvent == null)
 			OnLandEvent = new UnityEvent();
@@ -54,11 +60,12 @@ public class CharacterController2D : MonoBehaviour
 	{
 
 		//only control the player if grounded or airControl is turned on
-		if (Grounded || AirControl)
+		if (Grounded || !hook.enabled)
 		{
 			// Move the character by finding the target velocity
 			Vector3 targetVelocity = new Vector2(move * 10f, body.velocity.y);
 			// And then smoothing it out and applying it to the character
+			Velocity = body.velocity;
 			body.velocity = Vector3.SmoothDamp(body.velocity, targetVelocity, ref Velocity, MovementSmoothing);
 
 			// If the input is moving the player right and the player is facing left...
@@ -75,11 +82,13 @@ public class CharacterController2D : MonoBehaviour
 			}
 		}
 		// If the player should jump...
-		if (Grounded && jump)
+		if ((Grounded || player.GetComponent<Grapple>().hookJump) && jump)
 		{
 			// Add a vertical force to the player.
 			Grounded = false;
+			player.GetComponent<Grapple>().hookJump = false;
 			body.AddForce(new Vector2(0f, JumpForce));
+			player.GetComponent<Grapple>().disableHook();
 		}
 	}
 
@@ -88,7 +97,6 @@ public class CharacterController2D : MonoBehaviour
 	{
 		// Switch the way the player is labelled as facing.
 		FacingRight = !FacingRight;
-
 		// Multiply the player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
--- New Unity Project/Assets/Grapple.cs
+++ New Unity Project/Assets/Grapple.cs
@@ -1,35 +1,125 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Grapple : MonoBehaviour
 {
-    public GameObject grapple;
-    public GameObject Player;
-    GameObject curHook;
-    // Start is called before the first frame update
+    [SerializeField] private GameObject player;
+    //Rigidbody of the player
+    Rigidbody2D body;
+    //Grappling hook sprite
+    LineRenderer line;
+    //Joint that controls grapple physics
+    DistanceJoint2D joint;
+    //Location of the mouse when clicked
+    Vector3 targetPos;
+    //Raycast checking for rope collision with a wall
+    RaycastHit2D hit;
+    //Determines if the hook is being reeled in
+    bool reel = false;
+    //Max Grappling hook length
+    public float distance = 10f;
+    //All things the grappling hook can collide with
+    public LayerMask mask;
+    //Speed at which the hook reels in
+    public float step = .05f;
+    //Minumum length of the hook
+    public float minLength = 1f;
+    //If the hook has just been jumped off of
+    public bool hookJump = false;
+    int counter = 0;
+
+    // Use this for initialization
     void Start()
     {
-        curHook = Instantiate(grapple, Vector2.zero, Quaternion.identity);
-        curHook.GetComponent<HingeJoint2D>().connectedBody = Player.GetComponent<Rigidbody2D>();
-        curHook.SetActive(false);
+        body = GetComponent<Rigidbody2D>();
+        joint = GetComponent<DistanceJoint2D>();
+        line = GetComponent<LineRenderer>();
+        disableHook();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) && !curHook.activeSelf)
+        if (Input.GetKeyDown(KeyCode.E) && joint.enabled)
         {
-            Vector2 mouseLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Player.GetComponent<Rigidbody2D>().freezeRotation = false;
-            curHook.transform.position = mouseLocation;
-            curHook.SetActive(true);
+            reel = !reel;
         }
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetMouseButtonDown(0))
         {
-            curHook.SetActive(false);
-            Player.transform.rotation = Quaternion.Euler(0,0,0);
-            Player.GetComponent<Rigidbody2D>().freezeRotation = true;
+            //Find mouse position
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+            //Check for collisions between the player and the mouse position
+            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
+            //On contact with something with a rigid body
+            if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                joint.enabled = true;
+                Vector2 connectPoint = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+                connectPoint.x = connectPoint.x / hit.collider.transform.localScale.x;
+                connectPoint.y = connectPoint.y / hit.collider.transform.localScale.y;
+                joint.connectedAnchor = connectPoint;
+
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.distance = Vector2.Distance(transform.position, hit.point);
+
+                line.enabled = true;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, hit.point);
+            }
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            hookJump = true;
+        }
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            disableHook();
         }
     }
+    void FixedUpdate()
+    {
+        //Moves player towards the hook until it is destroyed
+        if (reel == true)
+        {
+            if (Vector2.Distance(joint.connectedAnchor, Vector2.MoveTowards(body.position, joint.connectedAnchor, 1)) > minLength)
+            {
+                pullInPlayer();
+            }
+            else
+            {
+                disableHook();
+            }
+        }
+        if(hookJump)
+        {
+            hookJump = false;
+        }
+        /*
+        if(line.enabled)
+        {
+            line.SetPosition(1, joint.connectedBody.transform.TransformPoint(joint.connectedAnchor));
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+
+            line.SetPosition(0, transform.position);
+        }
+        */
+    }
+    public void disableHook()
+    {
+        joint.enabled = false;
+        line.enabled = false;
+        reel = false;
+    }
+    public void pullInPlayer()
+    {
+        Vector3 speed = Vector3.MoveTowards(body.position, joint.connectedAnchor, step);
+        Vector3 Velocity = body.velocity;
+        body.velocity = Vector3.SmoothDamp(body.velocity, speed, ref Velocity, player.GetComponent<CharacterController2D>().MovementSmoothing);
+        joint.distance -= step * Time.deltaTime;
+    }
+
+
 }
